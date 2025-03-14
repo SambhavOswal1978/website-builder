e13a00fde6e03d5bae8be0ef5b9d4445b1dc85ac
Yash5108 <152095105+Yash5108@users.noreply.github.com>
2025-03-14 20:04:36 +0530

    2 templates removed, blog template edited

--- src/Pages/Templates/TemplatesPage.js
+++ src/Pages/Templates/TemplatesPage.js
@@ -25,7 +25,7 @@ const templates = [
     name: "Event",
     description: "Event announcement layout",
     file: "event-template.wbproj",
-    image: "/Templates/Event-cover.png",
+    image: "",
   },
   {
     name: "Blog",
